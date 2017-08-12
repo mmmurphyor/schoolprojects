3420949a97374a99f1b641bcb4480cc191cf250b
mmmurphyor <margret.murphy@oit.edu>
2017-08-12 00:30:17 -0700

    Add files via upload
    simple file transfer program.

---
+++ item66.py
@@ -0,0 +1,6 @@
+Python 3.5.2 (v3.5.2:4def2a2901a5, Jun 25 2016, 22:01:18) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+============ RESTART: C:\Users\Margret\Downloads\item66Margret.py ============
+Tue Jan 17 14:06:15 2017
+>>> 
